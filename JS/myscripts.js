a2d0025b896c613955e6192a55f975f90c0e5a58
Thaís <ttheinterpreter@gmail.com>
2022-04-07 18:40:54 -0300

    Starting to work with JS

---
+++ JS/myscripts.js
@@ -0,0 +1,12 @@
+let toggleNavStatus = false;
+
+function toggleNav(){
+    let getSidebar = document.querySelector('.nav-sidebar');
+    let getSidebarMenu = document.querySelector('.nav-sidebar aside');
+    let getSidebarLinks = document.querySelector('.nav-sidebar a');
+    let logoBtnMenu = document.querySelector('.btn-toggle-nav');
+}
+
+if(toggleNavStatus === false) {
+    getSidebarUl.style.visibility = "visible";
+}
