836de637845b1d3f7901547d944e694327634d23
Thaís <ttheinterpreter@gmail.com>
2022-04-08 11:04:08 -0300

    Experimenting with JS

--- JS/myscripts.js
+++ JS/myscripts.js
@@ -1,12 +1,25 @@
-let toggleNavStatus = false;
+// Get the modal
+ var modal = document.getElementById("myModal");
 
-function toggleNav(){
-    let getSidebar = document.querySelector('.nav-sidebar');
-    let getSidebarMenu = document.querySelector('.nav-sidebar aside');
-    let getSidebarLinks = document.querySelector('.nav-sidebar a');
-    let logoBtnMenu = document.querySelector('.btn-toggle-nav');
+//  Get the buttons that open the modal
+var btn = document.getElementById("myBtn");
+
+// Get the element that closes the modal
+var x = document.getElementById("close");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-if(toggleNavStatus === false) {
-    getSidebarUl.style.visibility = "visible";
+// When the user clicks on (x), close the modal
+x.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event){
+    if(event.target == modal) {
+        modal.style.display ="none";
+    }
 }
